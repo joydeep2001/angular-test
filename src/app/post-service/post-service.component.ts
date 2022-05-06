fa9c4c660e588fb7b2440932c1292067eb4879d6
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-05-06 18:25:41 +0530

    update2

--- src/app/post-service/post-service.component.ts
+++ src/app/post-service/post-service.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms'
+import { SubSink } from 'subsink';
 import { PostService } from '../post.service';
+import { RxStorage } from 'rxjs-storage';
+import { LocalStorage } from '@ngx-pwa/local-storage';
 
 @Component({
   selector: 'app-post-service',
@@ -8,24 +11,31 @@ import { PostService } from '../post.service';
   styleUrls: ['./post-service.component.css']
 })
 export class PostServiceComponent implements OnInit {
-
+  private subSink = new SubSink();
   contactForm:FormGroup;
+  rxjs:RxStorage;
+
   
-  constructor(private formBuilder: FormBuilder, private postService:PostService) { 
+  constructor(private formBuilder: FormBuilder, private postService:PostService, private localStorage:LocalStorage) { 
     this.contactForm = this.formBuilder.group({
       name: ['', Validators.required],
       job: ['', Validators.required]
-    })
+    });
+    this.rxjs = new RxStorage();
+    
   }
   onSubmit(): void {
-    this.postService.createUser(this.contactForm.value).subscribe((res:any) => {
-      console.table(res);
+    this.subSink.sink = this.postService.createUser(this.contactForm.value).subscribe((res:any) => {
+      this.rxjs.setItem(localStorage, "key", "val");
     });
-
     
   }
   ngOnInit(): void {
 
+  }
+  ngOnDestroy(){
+    console.log("unsubscribed");
+    this.subSink.unsubscribe();
   }
 
 }
